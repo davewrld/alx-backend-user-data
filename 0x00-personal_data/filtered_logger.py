bd0cef158d03eca5aa3b4bc71b3e79f4d0b30927
davewrld <keithdave23@gmail.com>
2024-11-08 09:22:12 +0000

    returns log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env/python3
+#!/usr/bin/env python3
 
 import re
 
 """Function that returns the message obfuscated"""
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str):
+def filter_datum(fields:list, redaction:str, message:str, separator:str) -> str:
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f'\\1={redaction}', message)
