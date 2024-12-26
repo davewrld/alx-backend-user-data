6040dd501167eecca716050733186d62614462cf
davewrld <keithdave23@gmail.com>
2024-12-26 11:17:20 +0000

    fixed annotation errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,6 @@ import re
 
 """Function that returns the message obfuscated"""
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
     pattern = '|'.join(f"{field}=.*?(?={separator}|$)" for field in fields)
     return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
