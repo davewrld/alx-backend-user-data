ef265c718fce092d5a8003e0a1315c1541896a9b
davewrld <keithdave23@gmail.com>
2024-12-26 11:34:21 +0000

    fixed pycodestyles error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,24 @@
 import re
 from typing import List
 
-"""Function that returns the message obfuscated"""
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+        fields (List[str]): Fields to obfuscate.
+        redaction (str): The string to replace field values with.
+        message (str): The log message.
+        separator (str): The field separator in the log message.
+
+    Returns:
+        str: The obfuscated log message.
+    """
     pattern = '|'.join(f"{field}=.*?(?={separator}|$)" for field in fields)
-    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+    return re.sub(
+        pattern,
+        lambda m: f"{m.group().split('=')[0]}={redaction}",
+        message
+    )
